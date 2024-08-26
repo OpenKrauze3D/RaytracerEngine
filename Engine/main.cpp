67c76e54cd681a0ffb286a8e1436ae0f4f13e67a
Kamil Krauze <72396466+KamilKrauze@users.noreply.github.com>
2024-08-26 10:27:05 +0100

    Added noise image creation

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -1,31 +1,40 @@
 #include <iostream>
+#include <random>
 
 #include "Core/Image.hpp"
 
 int main()
 {
     // Image
-    constexpr uint16_t WIDTH = 256;
-    constexpr uint16_t HEIGHT = 256;
-    constexpr uint8_t CHANNELS = 3;
+    constexpr uint16_t WIDTH = 2048;
+    constexpr uint16_t HEIGHT = 1024;
 
-    ImageSpec spec = ImageSpec(WIDTH, HEIGHT, 31);
-    Image img(spec);
 
-    // Render
-    for (int j = 0; j < HEIGHT; ++j) {
-        std::clog << "\rScanlines remaining: " << (HEIGHT - j) << ' ' << std::flush;
-        for (int i = 0; i < WIDTH; ++i) {
-            size_t idx = ((j * WIDTH) + i);
+    {
+        ImageSpec spec = ImageSpec(WIDTH, HEIGHT, 31);
+        Image img(spec);
 
-            img.m_data[idx].r = i / static_cast<double>(WIDTH);     // Red channel (U)
-            img.m_data[idx].g = j / static_cast<double>(HEIGHT); // Green channel (V)
-            img.m_data[idx].b = 0.0f;
+        std::mt19937_64 generator(0);
+        std::uniform_real_distribution<float> red(0, 1.0f);
+        std::uniform_real_distribution<float> green(0, 1.0f);
+        std::uniform_real_distribution<float> blue(0, 1.0f);
+
+        // Render
+        for (int j = 0; j < HEIGHT; j++) {
+            std::clog << "\rScanlines remaining: " << (HEIGHT - j) << ' ' << std::flush;
+            for (int i = 0; i < WIDTH; i++) {
+                size_t idx = ((j * WIDTH) + i);
+
+                img.m_data[idx].r = red(generator);     // Red channel (U)
+                img.m_data[idx].g = green(generator); // Green channel (V)
+                img.m_data[idx].b = blue(generator);
+            }
         }
-    }
-    std::clog << std::endl;
+        std::clog << std::endl;
 
-    img.writeToDisk("../test", true);
-    std::clog << "\rDone\t\t\t\t";
+        img.writeToDisk("../test", true);
+        std::clog << "\rDone\t\t\t\t";
+        std::cin.get();
+    }
     std::cin.get();
 }
