1935da5605e3dad985d192275b1ae2fcb4296332
KamilKrauze <72396466+KamilKrauze@users.noreply.github.com>
2025-12-30 17:51:23 +0000

    Sphere with normals.

--- src/Core/ray_3d.hpp
+++ src/Core/ray_3d.hpp
@@ -21,22 +21,30 @@ namespace rte
         vec3 dir = {0,0,0};
     };
 
-    constexpr bool hit_sphere(const Point3D& center, double radius, const Ray3D& r)
+    constexpr double hit_sphere(const Point3D& center, double radius, const Ray3D& r)
     {
         vec3 oc = center - r.origin();
-        auto a = dot(r.direction(), r.direction());
-        auto b = -2.0 * dot(r.direction(), oc);
-        auto c = dot(oc, oc) - radius*radius;
-        auto discriminant = b*b - 4*a*c;
-        return (discriminant >= 0);
+        auto a = r.direction().length_squared();
+        auto h = dot(r.direction(), oc);
+        auto c = oc.length_squared() - radius*radius;
+        auto discriminant = h*h - a*c;
+        // return (discriminant >= 0);
+        
+        if (discriminant < 0) {
+            return -1.0;
+        } else {
+            return (h - std::sqrt(discriminant) ) / a;
+        }
     }
     
     constexpr Colour ray_colour(const Ray3D& ray)
     {
-        if (hit_sphere({0,0,-1}, 0.5f, ray))
-        {
-            return Colour(1,0,0);
+        double t = hit_sphere(Point3D(0.2,-0.5,-1), 0.5, ray);
+        if (t > 0.0) {
+            vec3 N = unit_vector(ray.at(t) - vec3(0,0,-1));
+            return 0.5 * Point3D(N.x+1, N.y+1, N.z+1);
         }
+        
         vec3 unit_direction = unit_vector(ray.direction());
         const double lerp = 0.5*(unit_direction.y + 1.0);
         return (1.0-lerp)*Colour(1.0, 1.0, 1.0) + lerp*Colour(0.5, 0.7, 1.0);
